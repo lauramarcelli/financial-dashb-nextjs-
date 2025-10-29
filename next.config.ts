43b1341f921f2d4433c46d1443f670dd6adda34a
lauramarcelli <laura.marcelli@gmail.com>
2025-10-29 11:17:30 -0300

    adding some pagination features

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    ppr: 'incremental'
-  }
+    typedRoutes: true,
+  },
 };
 
 export default nextConfig;
